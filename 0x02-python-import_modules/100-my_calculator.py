53e9dfc56349afa629b1e01249ce9583bf13d8da
M3siCS <hadeelkaddy86@gmail.com>
2024-01-04 20:48:07 -0800

    uploded

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
         print("{} - {} = {}".format(a, b, sub(a, b)))
     elif op == "*":
         print("{} * {} = {}".format(a, b, mul(a, b)))
-                                                                                                    elif op == "/":
-                                                                                                                print("{} / {} = {}".format(a, b, div(a, b)))
-                                                                                                                    else:
-                                                                                                                                print("Unknown operator. Available operators: +, -, * and /")
-                                                                                                                                        exit(1)
+    elif op == "/":
+        print("{} / {} = {}".format(a, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
