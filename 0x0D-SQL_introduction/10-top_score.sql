bf3bf792efaa20d1e90431615335ce965c53e3d5
M3siCS <hadeelkaddy86@gmail.com>
2024-03-12 19:15:04 -0700

    up

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- List all records of the table second_table from the database hbtn_0c_0
+-- Display both the score and the name, ordered by score (top first)
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
+
---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,7 @@
+-- List all records with a score >= 10 in the table second_table from the database hbtn_0c_0
+-- Display both the score and the name, ordered by score (top first)
+SELECT score, name
+FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
+
