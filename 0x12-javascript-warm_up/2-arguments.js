c92d5b5586f857f63f3d1b1e776080c3e8132c86
M3siCS <hadeelkaddy86@gmail.com>
2024-04-13 16:53:30 -0700

    uploddd

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,14 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-const argsLength = argv.length;
-console.log(argsLength === 2 ? 'No argument' : argsLength === 3 ? 'Argument found' : 'Argument found');
+const argsLength = process.argv.length;
+
+switch (argsLength) {
+    case 2:
+        console.log("No argument");
+        break;
+    case 3:
+        console.log("Argument found");
+        break;
+    default:
+        console.log("Arguments found");
+}
+
