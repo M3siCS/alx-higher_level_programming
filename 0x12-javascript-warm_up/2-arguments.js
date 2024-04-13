e0b441de4f649e99872c4187c63433bc57530d5f
M3siCS <hadeelkaddy86@gmail.com>
2024-04-13 16:58:48 -0700

    uploddd

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,13 +2,12 @@
 const argsLength = process.argv.length;
 
 switch (argsLength) {
-    case 2:
-        console.log("No argument");
-        break;
-    case 3:
-        console.log("Argument found");
-        break;
-    default:
-        console.log("Arguments found");
+	case 2:
+		console.log("No argument");
+		break;
+	case 3:
+		console.log("Argument found");
+		break;
+	default:
+		console.log("Arguments found");
 }
-
