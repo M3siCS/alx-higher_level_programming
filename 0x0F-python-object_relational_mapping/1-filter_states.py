c3081074f3d806e451246c61dc9ad49eaebdb186
M3siCS <hadeelkaddy86@gmail.com>
2024-04-14 17:46:00 -0700

    uploddd

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,29 +13,14 @@ if __name__ == '__main__':
     Access to the database and get the states
     from the database.
     """
-    mysql_username = argv[1]
-    mysql_password = argv[2]
-    db_name = argv[3]
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-    try:
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=mysql_username,
-            passwd=mysql_password,
-            db=db_name,
-            charset="utf8"
-        )
-    except MySQLdb.Error as e:
-        print("Error connecting to database: {}".format(e))
-        exit(1)
-
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
-    rows = cursor.fetchall()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states \
+                 WHERE name LIKE BINARY 'N%' \
+                 ORDER BY states.id ASC")
+    rows = cur.fetchall()
 
     for row in rows:
         print(row)
-
-    cursor.close()
-    db.close()
